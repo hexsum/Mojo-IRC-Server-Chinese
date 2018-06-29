6711435e5002565e81017368fdd5dec7c038f6ea
sjdy521 <sjdy521@163.com>
2018-06-29 11:27:08 +0800

    2018-06-29 Mojo::IRC::Server::Chinese v1.8.2 发布

--- lib/Mojo/IRC/Server/Chinese.pm
+++ lib/Mojo/IRC/Server/Chinese.pm
@@ -1,6 +1,6 @@
 package Mojo::IRC::Server::Chinese;
 use strict;
-$Mojo::IRC::Server::Chinese::VERSION = "1.8.1";
+$Mojo::IRC::Server::Chinese::VERSION = "1.8.2";
 use Encode;
 use Encode::Locale;
 use Carp;
